c3798a2f66d35829128d8343ee729af518711013
mac <imac@MacBook-Air-imac.local>
2025-11-04 16:14:36 +0300

    Fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  publicDir: 'public',
   base: '/zadanie5/',
   build: {
     outDir: 'dist',
